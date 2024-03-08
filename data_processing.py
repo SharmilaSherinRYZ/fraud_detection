471b95eaa38705ba9fbc76ae0693962ce9c6decb
SharmilaSherinRYZ <sharmilasherinpattu@gmail.com>
2024-03-08 17:29:21 +0530

    add another fun

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,13 @@
 print('adding all the data processing related to fraud_detection')
 
-print("Hello")
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return c
+print('the sum function')
+
+def subtract(a,b):
+    d=a*b
+    return a
+print('the multiplication fun')
