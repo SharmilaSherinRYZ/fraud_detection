e5c2fa78c6764a4728608f30c8d7a0d791b39974
SharmilaSherinRYZ <sharmilasherinpattu@gmail.com>
2024-03-09 07:38:50 +0530

    modified ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print('adding all ml related codes here')
+
+def model1():
+    print('adding the mlcode for model 1')
+
+def model2():
+    print('adding the mlcode for model 2')
+
+
+def model3():
+    print('adding the mlcode for model 3')
